7f58251b68f92236b0a33de331b6383290aea9e4
Vadim Kasatkin <penguin2804@gmail.com>
2022-05-16 11:48:55 +0400

    refactor: owner middleware to class link in Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'owner' => 'App\Http\Middleware\OwnerMiddleware',
+        'owner' => \App\Http\Middleware\OwnerMiddleware::class,
         'book_access' => \App\Http\Middleware\BookAccess::class
     ];
 }
